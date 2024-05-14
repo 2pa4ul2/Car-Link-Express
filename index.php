4f94c11e0b798ae9d2381c06cb05a637d4a3677c
Paul Adrian <pauladrian0224@gmail.com>
2024-05-14 19:21:03 +0800

    Added php

---
+++ index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img class="logo" src="assets/logo.png" alt="">
+        <ul>
+            <li><a class="anc" href="#">About</a></li>
+            <li><a class="anc" href="#">Contacts</a></li>
+        </ul>
+    </header>
+    <div class="tagline">
+        <h1>Driving Trust, Delivering excellence</h1>
+        <p>Your Premier Car Distribution Partner</p>
+    </div>
+    <div id="nav-bar">
+            <ul class="nav-ul">
+                <li><a class="nav-button" href="#">Home</a></li>
+                <li><a class="nav-button" href="#">Supplier</a></li>
+                <li><a class="nav-button" href="#">Product</a></li>
+                <li><a class="nav-button" href="#">Categories</a></li>
+            </ul>
+    </div>
+    <div class="hero">
+        <!-- <video autoplay loop muted plays-inline class="video-back">
+            <source src="assets/bgvid.mp4" type="video/mp4">
+        </video> -->
+        <img class="video-back" src="assets/bgimg.jpg" alt="">
+    </div>
+</body>
+</html>
